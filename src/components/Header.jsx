4bce5e36929d2b86091bd50c9aede702c9135e63
Harsh <koladkarharsh@gmail.com>
2025-03-30 19:46:33 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,19 +31,19 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm h-14 ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="/">
-          <img src={brainwave} width={190} height={20} alt="StyleAura" />
+      <div className="flex items-center px-4 lg:px-6 xl:px-8 h-14">
+        <a className="block w-[9rem] xl:mr-4" href="/">
+          <img src={brainwave} width={150} height={16} alt="StyleAura" />
         </a>
 
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed top-[4rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
@@ -51,13 +51,13 @@ const Header = () => {
                 key={item.id}
                 href={item.url}
                 onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                className={`block relative font-code text-base uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                } px-4 py-4 md:py-5 lg:-mr-0.25 lg:text-sm lg:font-semibold ${
                   item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                } lg:leading-5 lg:hover:text-n-1 xl:px-7`}
               >
                 {item.title}
               </a>
@@ -69,7 +69,7 @@ const Header = () => {
 
         <a
           href="#signup"
-          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+          className="button hidden mr-4 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
           New account
         </a>
@@ -79,7 +79,7 @@ const Header = () => {
 
         <Button
           className="ml-auto lg:hidden"
-          px="px-3"
+          px="px-2"
           onClick={toggleNavigation}
         >
           <MenuSvg openNavigation={openNavigation} />
